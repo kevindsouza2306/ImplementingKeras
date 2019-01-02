9b9f197df7d1be00abbd8657255e6fe647edc98a
kevin <kevindsouza2306@gmail.com>
2019-01-03 01:48:50 +0530

    Initial commit

---
+++ keras_mnist.py
@@ -0,0 +1,13 @@
+__author__ = 'kevin'
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import datasets
+from keras.layers.core import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+np.random.randn(1)
