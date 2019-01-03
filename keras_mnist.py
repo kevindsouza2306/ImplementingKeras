7e65c6a1cbf29946d000e73a51c740c3c30f8799
Administrator <kevindsouza2306@gmail.com>
2019-01-03 17:53:30 +0530

    Keras_mnist.py code completion

--- keras_mnist.py
+++ keras_mnist.py
@@ -17,8 +17,38 @@ args = vars(ap.parse_args())
 print("[INFO] Loading MNIST (full) dataset....")
 datasets = datasets.fetch_mldata("MNIST Original")
 data = datasets.data.astype("float") / 255.0
-(trainX, testX, trainY, testY) = train_test_split(data, datasets.target, test_size=0.25)\
+(trainX, testX, trainY, testY) = train_test_split(data, datasets.target, test_size=0.25)
 
 lb = LabelBinarizer
 testY = lb.fit_transform(testY)
 trainY = lb.fit_transform(trainY)
+
+model = Sequential()
+model.add(Dense(256, input_shape=(784,), activation="sigmoid"))
+model.add(Dense(128, activation="sigmoid"))
+model.add(Dense(10, activation="sigmoid"))
+
+print("[INFO] Training Network...")
+
+sgd = SGD(0.01)
+
+model.compile(loss="categorial_crossentropy", optimizer=sgd, metrics=["accuracy"])
+
+H = model.fit(trainX, trainY, validation_data=(testX, testY), epochs=100, batch_size=128)
+
+print("[INFO] evaluating network")
+predection = model.predict(testX, batch_size=128)
+print(classification_report(testY.argmx(axis=1), predection.argmax(axis=1), target_names=[str(x) for x in lb.classes_]))
+
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 100), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, 100), H.history["val_acc"], label="val_acc")
+
+plt.title("Trainning Loss and Accuracy")
+plt.xlable("Epoch #")
+plt.ylable("Loss/Accuracy")
+plt.legend()
+plt.savefig(args("output"))
